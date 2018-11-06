f3a61909435c8138d39e7dbe99ed3e03446bd16a
Angelos Theodorakopoulos <angtheod@gmail.com>
2018-11-06 11:15:40 +0200

    Another identation fix.

--- admin/js/admin_search_ml.js
+++ admin/js/admin_search_ml.js
@@ -178,7 +178,7 @@ function scroll_ml_images() {
         scroll_more = 1;
         var $ml_current_page = $( '#ml_current_page' );
         var next_page = parseInt( $ml_current_page.val() ) + 1;
-		$ml_current_page.val( next_page );
+        $ml_current_page.val( next_page );
         $( '#ml_loader', $('.supports-drag-drop:last-of-type') ).addClass('is-active');
 
         $.ajax({
